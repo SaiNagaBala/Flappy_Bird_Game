3b9f9728210db9b20bf80f1544d0e7aaedd267fd
SaiNagaBala <saibala.k@xcubelabs.com>
2022-03-03 16:27:46 +0530

    Bird Rotation is added

---
+++ Assets/BirdMovement.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float birdSpeed;
+    public int angle;
+    public int maxAngle;
+    public int minAngle;
+    private void Awake()
+    {
+        gameObject.AddComponent<Rigidbody2D>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            // create bird movement
+            BirdFlapAndJump();
+        }
+        BirdRotation();
+    }
+
+    private void BirdRotation()
+    {
+        if (rb.velocity.y > 0)
+        {
+            if (angle <= maxAngle)
+            {
+                angle = angle + 4;
+            }
+        }
+        else if (rb.velocity.y < 0)
+        {
+            if (angle >= minAngle)
+            {
+                angle = angle - 4;
+            }
+        }
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    private void BirdFlapAndJump()
+    {
+        rb.velocity = Vector2.zero;
+        rb.velocity = new Vector2(rb.velocity.x, birdSpeed);
+    }
+}
